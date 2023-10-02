945b7491c2fd2838c4a415f171d09d7a060c2957
Ibrahim <ebrahimmsafar@gmail.com>
2023-10-02 17:33:26 +0300

    add3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./products.js";
+import products from "./products.js";
 
 function App() {
+  const productList = products.map((product) => {
+    return (
+      <div className="imgprod">
+        <img src={product.img} width="200px" />
+        <h3> {product.name}</h3>
+        <p> {product.price}</p>
+      </div>
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contanr">
+      <h1>Nike app</h1>
+      <h3>the Best app in the word </h3>
+      <img
+        width="600px"
+        src="https://www.psychologies.co.uk/wp-content/uploads/sites/3/2014/10/iStock_000018517104_big_smile-1.jpg"
+      ></img>
+      <div className="prodlist">{productList}</div>
     </div>
   );
 }
 
 export default App;
+
+// {/* <img src={logo} className="App-logo" alt="logo" />
+// <p>
+//   Edit <code>src/App.js</code> and save to reload.
+// </p>
+// {/* <a
+//   className="App-link"
+//   href="https://reactjs.org"
+//   target="_blank"
+//   rel="noopener noreferrer"
+// >
+//   Learn React
+// </a> */}
+// </header>
---
+++ src/products.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    name: "blak nike",
+    price: "10KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/49b277b3-9c7c-4004-a400-be54ad53c31b/md-valiant-baby-toddler-shoes-RJGjJs.png",
+  },
+  {
+    name: "blue nike",
+    price: "15KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
+  },
+  {
+    name: "blue nike",
+    price: "15KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
+  },
+  {
+    name: "blue nike",
+    price: "15KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
+  },
+  {
+    name: "blue nike",
+    price: "15KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
+  },
+  {
+    name: "blue nike",
+    price: "15KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
+  },
+  {
+    name: "blue nike",
+    price: "15KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
+  },
+  {
+    name: "blue nike",
+    price: "15KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
+  },
+  {
+    name: "blue nike",
+    price: "15KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
+  },
+  {
+    name: "blue nike",
+    price: "15KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
+  },
+  {
+    name: "blue nike",
+    price: "15KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
+  },
+  {
+    name: "blue nike",
+    price: "15KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
+  },
+  {
+    name: "blue nike",
+    price: "15KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
+  },
+  {
+    name: "blue nike",
+    price: "15KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
+  },
+];
+
+export default products;

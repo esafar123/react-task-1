e77699b7aadc254052ddf32e5623c17e286966a0
Ibrahim <ebrahimmsafar@gmail.com>
2023-10-02 20:24:26 +0300

    done

--- src/products.js
+++ src/products.js
@@ -10,64 +10,9 @@ const products = [
     img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
   },
   {
-    name: "blue nike",
-    price: "15KD",
-    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
-  },
-  {
-    name: "blue nike",
-    price: "15KD",
-    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
-  },
-  {
-    name: "blue nike",
-    price: "15KD",
-    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
-  },
-  {
-    name: "blue nike",
-    price: "15KD",
-    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
-  },
-  {
-    name: "blue nike",
-    price: "15KD",
-    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
-  },
-  {
-    name: "blue nike",
-    price: "15KD",
-    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
-  },
-  {
-    name: "blue nike",
-    price: "15KD",
-    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
-  },
-  {
-    name: "blue nike",
-    price: "15KD",
-    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
-  },
-  {
-    name: "blue nike",
-    price: "15KD",
-    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
-  },
-  {
-    name: "blue nike",
-    price: "15KD",
-    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
-  },
-  {
-    name: "blue nike",
-    price: "15KD",
-    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
-  },
-  {
-    name: "blue nike",
-    price: "15KD",
-    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78d5b356-df0e-4a91-aea9-996225c21ce1/dunk-low-shoes-t5v34c.png",
+    name: "blue x nike",
+    price: "18KD",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/884a96f9-f579-415b-89b6-5bf14f80259b/air-max-90-toggle-se-baby-toddler-shoes-6b3kng.png",
   },
 ];
 
